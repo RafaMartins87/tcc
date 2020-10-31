024ce90de4c83a3bddc646067ebe8428c865bde9
RafaMartins87 <rafael.martins87@outlook.com>
2020-10-30 23:23:16 -0300

    melhorando

--- srcTCC/tcc.py
+++ srcTCC/tcc.py
@@ -8,7 +8,12 @@ import pyodbc
 app = Flask(__name__)
 CORS(app)
 
-
+# server = 'DESKTOP-6CP0SSO' 
+#     database = 'TCC2' 
+#     username = 'rafael'
+#     password = 'root'
+#     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+#     cursor = cnxn.cursor()
 
 class Consulta:
     def __init__(self, nomePaciente, convenio, servico,datahora,valor):
@@ -48,7 +53,59 @@ def consultas():
         'title':'Sistema Odonto Teste'
     }
 
-    # nomePaciente = request.form['paciente']
+    server = 'DESKTOP-6CP0SSO' 
+    database = 'TCC2' 
+    username = 'rafael'
+    password = 'root'
+    cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+    cursor = cnxn.cursor()
+
+    return render_template('rafa.html', params=params,)
+
+@app.route('/dentistas', methods=['POST','GET'])
+def dentistas():
+    result = {}
+
+    if request.method == 'POST':
+        result = request.form
+
+    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-JCQQMHI/SQLEXPRESS;DATABASE=DW_TCC')
+    cursor = conn.cursor()
+
+    cursor.execute("INSERT INTO DW_TCC (CRO, EMAIL_DENT) VALUES (1234,'USER@USER.COM')")
+    cursor.commit()
+    
+    title = 'Cadastro - Dentistas'
+
+    return render_template('dentistas.html', result=result, title=title)
+    
+@app.route('/pacientes')
+def pacientes():
+    return render_template('pacientes.html')
+
+@app.route('/convenios')
+def convenios():
+    return render_template('convenios.html')
+
+@app.route('/servicos')
+def servicos():
+    return render_template('servicos.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
+
+
+
+
+
+
+
+
+
+
+########################################################################################################
+# nomePaciente = request.form['paciente']
     # convenio = request.form['convenio']
     # servico = request.form['servico']
     # datahora = request.form['datahora']
@@ -71,41 +128,3 @@ def consultas():
     #     print ('Inserted Product key is ' + str(row[0]))
     #     row = cursor.fetchone()
     #print(consulta)
-    return render_template('rafa.html', params=params,)
-
-@app.route('/dentistas', methods=['POST','GET'])
-def dentistas():
-    result = {}
-
-    if request.method == 'POST':
-        result = request.form
-
-    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-JCQQMHI/SQLEXPRESS;DATABASE=DW_TCC')
-    cursor = conn.cursor()
-
-    cursor.execute("INSERT INTO DW_TCC (CRO, EMAIL_DENT) VALUES (1234,'USER@USER.COM')")
-    cursor.commit()
-    
-    title = 'Cadastro - Dentistas'
-
-    # params = {
-    #     'title':'Cadastro - Dentistas'
-    # }
-
-    return render_template('dentistas.html', result=result, title=title)
-    
-@app.route('/pacientes')
-def pacientes():
-    return render_template('pacientes.html')
-
-@app.route('/convenios')
-def convenios():
-    return render_template('convenios.html')
-
-@app.route('/servicos')
-def servicos():
-    return render_template('servicos.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
