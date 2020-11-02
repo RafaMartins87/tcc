4c6e400939d0fdb3cef6e250f9af5e51e6ae347a
RafaMartins87 <rafael.martins87@outlook.com>
2020-11-02 15:29:57 -0300

    parte 1 feita

--- srcTCC/tcc.py
+++ srcTCC/tcc.py
@@ -8,6 +8,13 @@ import pyodbc
 app = Flask(__name__)
 CORS(app)
 
+# server = 'DESKTOP-6CP0SSO' 
+#     database = 'TCC2' 
+#     username = 'rafael'
+#     password = 'root'
+#     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+#     cursor = cnxn.cursor()
+
 class Consulta:
     def __init__(self, nomePaciente, convenio, servico,datahora,valor):
         self.nomePaciente = nomePaciente
@@ -24,11 +31,14 @@ def home():
         'title':'Sistema Odonto Teste'
     }
 
-    
+    server = 'DESKTOP-6CP0SSO' 
+    database = 'TCC2' 
+    username = 'rafael'
+    password = 'root'
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = cnxn.cursor()
 
-    listaHome = cursor.execute("SELECT top 5 NM_PAC,p.CONVENIO,DS_SERV,c.DT_CONSULTA, c.VALOR_CONSULTA FROM F_CONSULTAS C INNER JOIN D_PACIENTES P ON P.ID_PACIENTE = C.ID_PACIENTE INNER JOIN D_SERVICOS S ON C.ID_SERVICO = S.ID_SERVICO INNER JOIN	D_CONVENIOS con ON con.ID_CONVENIO= c.ID_CONVENIO")
+    listaHome = cursor.execute("SELECT NM_PAC,p.CONVENIO,DS_SERV,c.DT_CONSULTA, c.VALOR_CONSULTA FROM F_CONSULTAS C INNER JOIN D_PACIENTES P ON P.ID_PACIENTE = C.ID_PACIENTE INNER JOIN D_SERVICOS S ON C.ID_SERVICO = S.ID_SERVICO INNER JOIN	D_CONVENIOS con ON con.ID_CONVENIO= c.ID_CONVENIO ORDER BY C.DT_CONSULTA DESC")
 
     for item in listaHome:  
         consulta = Consulta(item.NM_PAC, item.CONVENIO, item.DS_SERV,item.DT_CONSULTA,item.VALOR_CONSULTA)
@@ -36,24 +46,33 @@ def home():
 
     return render_template('rafa.html', params=params, consultas = listaConsultas)
 
-@app.route('/consultas', methods=['GET','POST'])
+@app.route('/consultas', methods=['POST'])
 def consultas():
     print('entrou metodo')
 
-    
+    server = 'DESKTOP-6CP0SSO' 
+    database = 'TCC2' 
+    username = 'rafael'
+    password = 'root'
     cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
     cursor = cnxn.cursor()
-
-    idConsulta = 1000
+    print('conectou banco ')
+    idConsulta = cursor.execute("SELECT MAX(ID_CONSULTA) FROM F_CONSULTAS").fetchval() 
     idConsulta = idConsulta + 1
+    print('pegou id')
+    print(idConsulta)
+
     nomePaciente = request.form['paciente']
+    print(nomePaciente)
     #VERIFICAR SE NOME EXISTE NA BASE 
     #SE SIM BUSCAR ID E GUARDAR, SE NÃO PRINTA MENSAGEM 
     
     count = cursor.execute("SELECT COUNT(*) FROM D_PACIENTES WHERE NM_PAC LIKE ?",nomePaciente).fetchval()
     if count>=1:
-        idPac = cursor.execute("SELECT ID_PACIENTE FROM D_PACIENTES WHERE NM_PAC LIKE ?").fetchval()
-
+        idPac = cursor.execute("SELECT ID_PACIENTE FROM D_PACIENTES WHERE NM_PAC LIKE ?",nomePaciente).fetchval()
+    print('pegou nome paciente - acessou form')
+    
+    print(idPac)
     convenio = request.form['convenio']
     count = cursor.execute("SELECT COUNT(*) FROM D_CONVENIOS WHERE DS_CONV LIKE ?",convenio).fetchval()
     if count>=1:
@@ -67,12 +86,13 @@ def consultas():
     datahora = request.form['datahora']
     valor = request.form['valor']
 
-    p = [idConsulta,idPac, idConvenio, idServico, datahora, valor]
-
-    cursor.execute("INSERT INTO F_CONSULTAS (ID_CONSULTA, ID_DENTISTA, ID_CONVENIO, ID_SERVICO, DT_CONSULTA,VALOR_CONSULTA, ID_PACIENTE) VALUES(?,?,?,?,?,?,?)",p)
-
-    return render_template('rafa.html')
-
+    p = [idConsulta, 1, idConvenio, idServico, datahora, valor, idPac]
+    print(p)
+    cursor.execute("INSERT INTO F_CONSULTAS (ID_CONSULTA, ID_DENTISTA, ID_CONVENIO, ID_SERVICO, DT_CONSULTA,VALOR_CONSULTA, ID_PACIENTE) VALUES(?,?,?,?,?,?,?)",idConsulta, 1, idConvenio, idServico, datahora, 0, idPac)
+    cursor.commit()
+    print('fez insert')
+    #return render_template('rafa.html')
+    return redirect('/')
 @app.route('/dentistas', methods=['POST','GET'])
 def dentistas():
     result = {}
